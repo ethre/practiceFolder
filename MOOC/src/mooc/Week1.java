bbed483f2baa4072077627e0bd4dcbbbffbf66cb
ethre <ethre@hotmail.com>
2019-03-10 16:52:16 +0000

    third commit

--- MOOC/src/mooc/Week1.java
+++ MOOC/src/mooc/Week1.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 public class Week1 {
 
 	public static void main(String[] args) {
-	
+	/*
 	Scanner scanner = new Scanner(System.in);
 	
 	int number= Integer.parseInt(scanner.nextLine());
-	
+	*/
+		
 	/*
 	while(number<=number2) {
-		
 		System.out.println(number);
 		number++;
 	}
@@ -27,19 +27,44 @@ public class Week1 {
 	System.out.println("sum is "+ sum);
 	*/
 	
-	
+	/*
 	int i=0;
 	int factorialSum =0;
 	while(i<=number) {
 		if(i==0) {
 		factorialSum=1;
 		i++;
-		}else {
+	}else{
 		factorialSum*=i;
 		i++;
-		}
+	}
 	}
 	System.out.println(factorialSum);
+	*/
+	/*
+	int i=0;
+	while(i<=number) { 
+	
+	greet();
+	i++;
+	}
 	
 	}
+	public static void greet() {
+		System.out.println("whats up amigo");
+	}
+	*/
+	
+	printStars(5);
+	
+	}
+	
+	public static void printStars(int amount) {
+		int i=0;
+		while(i<amount) {
+			System.out.print("*");
+			i++;
+		}
+	}
+	
 }
