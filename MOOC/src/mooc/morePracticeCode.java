3d04d1227cedf472a096c84f86a89060e3ad2955
ethre <ethre@hotmail.com>
2019-03-11 01:08:05 +0000

    8 commit

--- MOOC/src/mooc/morePracticeCode.java
+++ MOOC/src/mooc/morePracticeCode.java
@@ -8,28 +8,27 @@ public class morePracticeCode {
 	public static void main(String[] args) {
 	
 	Scanner scanner = new Scanner(System.in);
-	int number = Integer.parseInt(scanner.nextLine());
+	
 	int randomNumber=0;	
 	
 	randomNumber = drawNumber();
 	
-	guess();
+	for(int i=0; i<3;i++) {
+	int number = Integer.parseInt(scanner.nextLine());
+	System.out.println(randomNumber);
+	if(number==randomNumber) {
+		System.out.println("thats the right guess");
+	}else if(number<randomNumber) {
+		System.out.println("the number is greater than user thought");
+	}else {
+		System.out.println("the number is lesser than user thought");
+		}
+	}
 	}
 	
 	public static int drawNumber() {
 		int rGen = (int)(Math.random()*100);
 		return rGen;
 	}
-	
-	public static void guess() {
-		if(number==randomNumber) {
-			System.out.println("thats the right guess");
-		}else if(number<randomNumber) {
-			System.out.println("the number is less than given");
-		}else {
-			System.out.println("the number is more than given");
-		}
-	}
-	
 	
 }
